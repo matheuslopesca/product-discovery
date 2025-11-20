87c39aa937e36023b56c422d3485142f96012d2d
Matheus Carvalho <matheuslopescarvalho@gmail.com>
2025-11-20 17:10:13 -0300

    Mensagem para familia

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 function Home() {
     return <h1> Estudar tanto pra que ? <br></br>
     Pra conseguir melhorar a vida de quem importa! <br></br>
-    Familia e amigos! <br></br>
-    Voces sao minha motivacao! <br></br>
+    Familia! <br></br>
+    Voces são minha motivacao! <br></br>
     Amo voces! <br></br>
     </h1>
 }
